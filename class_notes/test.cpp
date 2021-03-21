c6e218380b6d10beb32cafcea6f9aed4c62c9070
Sean Kunz <skunz598@gmail.com>
2021-03-21 12:55:22 -0400

    anotha one

---
+++ class_notes/test.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(int &x, int &y) {
+    x = x + y;
+    y = x - y;
+    x = x - y;
+}
+
+int main() {
+    int i;
+    int a[3];
+    i = 1;
+    a[0] = 2;
+    a[1] = 1;
+    a[2] = 0;
+    //swap(i, a[i]);
+    i = i + a[i];
+    a[i] = i - a[i];
+    i = i - a[i];
+    printf("%d %d %d %d\n", i, a[0], a[1], a[2]);
+    a[i] = a[i] + a[i];
+    a[i] = a[i] - a[i];
+    a[i] = a[i] - a[i];
+    printf("%d %d %d\n", a[0], a[1], a[2]);
+    return 0;
+}
