a010836282450b81871baadf9b017e48e4730578
Sean Kunz <skunz598@gmail.com>
2021-04-19 16:09:02 -0400

    another one

--- kunz-p4/assignment4.hs
+++ kunz-p4/assignment4.hs
@@ -10,3 +10,15 @@ minhelper (h:t) m
 minlist [] = 0
 minlist [h] = h
 minlist (h:t) = minhelper (h:t) h
+
+-- Problem 2
+
+--delete _ [] = []
+--delete 1 (h:t) = t
+--delete k (h:t) = [h] ++ delete (k-1) t
+
+delhelp _ _ [] = []
+delhelp k 1 (h:t) = delhelp k k t
+delhelp k s (h:t) = [h] ++ (delhelp k (s-1) t)
+
+delete k lt = delhelp k k lt
